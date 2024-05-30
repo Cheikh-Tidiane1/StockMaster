9e51e863f53a6ab7e8905f8f47c895b1aab7a3f8
cheikh tidiane <cheikh951812@gmail.com>
2024-05-30 22:50:37 +0200

    feat(services): ventesController

---
+++ src/main/java/com/tid/StockMaster/services/impl/VentesServiceImpl.java
@@ -0,0 +1,111 @@
+package com.tid.StockMaster.services.impl;
+
+import com.tid.StockMaster.dto.LigneVenteDto;
+import com.tid.StockMaster.dto.VentesDto;
+import com.tid.StockMaster.exception.EntityNotFoundException;
+import com.tid.StockMaster.exception.ErrorCodes;
+import com.tid.StockMaster.exception.InvalidEntityException;
+import com.tid.StockMaster.model.Article;
+import com.tid.StockMaster.model.LigneVente;
+import com.tid.StockMaster.model.Ventes;
+import com.tid.StockMaster.repository.ArticleRepository;
+import com.tid.StockMaster.repository.LigneVenteRepository;
+import com.tid.StockMaster.repository.VentesRepository;
+import com.tid.StockMaster.services.VentesService;
+import com.tid.StockMaster.validator.VentesValidator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class VentesServiceImpl implements VentesService {
+
+    private ArticleRepository articleRepository;
+    private VentesRepository  ventesRepository;
+    private LigneVenteRepository ligneVenteRepository;
+
+    public VentesServiceImpl(ArticleRepository articleRepository, VentesRepository ventesRepository, LigneVenteRepository ligneVenteRepository) {
+        this.articleRepository = articleRepository;
+        this.ventesRepository = ventesRepository;
+        this.ligneVenteRepository = ligneVenteRepository;
+    }
+
+    @Override
+    public VentesDto save(VentesDto dto) {
+
+        List<String> errors = VentesValidator.validate(dto);
+        if (!errors.isEmpty()) {
+            log.error("Ventes n'est pas valide");
+            throw new InvalidEntityException("L'objet vente n'est pas valide", ErrorCodes.VENTE_NOT_VALID, errors);
+        }
+
+        List<String> articleErrors = new ArrayList<>();
+
+        dto.getLigneVentes().forEach(ligneVenteDto -> {
+            Optional<Article> article = articleRepository.findById(ligneVenteDto.getArticle().getId());
+            if (article.isEmpty()) {
+                articleErrors.add("Aucun article avec l'ID " + ligneVenteDto.getArticle().getId() + " n'a ete trouve dans la BDD");
+            }
+        });
+
+        if (!articleErrors.isEmpty()) {
+            log.error("Un ou plusieurs articles n'ont pas ete trouve dans la BDD, {}", errors);
+            throw new InvalidEntityException("Un ou plusieurs articles n'ont pas ete trouve dans la BDD", ErrorCodes.VENTE_NOT_VALID, errors);
+        }
+
+        Ventes savedVentes = ventesRepository.save(VentesDto.toEntity(dto));
+
+        dto.getLigneVentes().forEach(ligneVenteDto -> {
+            LigneVente ligneVente = LigneVenteDto.toEntity(ligneVenteDto);
+            ligneVente.setVente(savedVentes);
+            ligneVenteRepository.save(ligneVente);
+        });
+
+        return VentesDto.fromEntity(savedVentes);
+    }
+
+    @Override
+    public VentesDto findById(Integer id) {
+        if (id == null) {
+            log.error("Ventes ID is NULL");
+            return null;
+        }
+        return ventesRepository.findById(id)
+                .map(VentesDto::fromEntity)
+                .orElseThrow(() -> new EntityNotFoundException("Aucune vente n'a ete trouve dans la BDD", ErrorCodes.VENTE_NOT_FOUND));
+    }
+
+    @Override
+    public VentesDto findByCode(String code) {
+        if (!StringUtils.hasLength(code)) {
+            log.error("Vente CODE is NULL");
+            return null;
+        }
+        return ventesRepository.findVentesByCode(code)
+                .map(VentesDto::fromEntity)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "Aucune vente client n'a ete trouve avec le CODE " + code, ErrorCodes.VENTE_NOT_VALID
+                ));
+    }
+
+    @Override
+    public List<VentesDto> findAll() {
+        return ventesRepository.findAll().stream()
+                .map(VentesDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void delete(Integer id) {
+        if (id == null) {
+            log.error("Vente ID is NULL");
+            return;
+        }
+        ventesRepository.deleteById(id);
+    }
+}
