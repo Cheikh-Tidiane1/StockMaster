d1503a614f83093c32224ff6ffbe5b53a26c3cfb
cheikh tidiane <cheikh951812@gmail.com>
2024-06-26 19:43:02 +0200

    feat(service): add UserDetailsService

--- src/main/java/com/tid/StockMaster/config/SecurityConfiguration.java
+++ src/main/java/com/tid/StockMaster/config/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.tid.StockMaster.config;
 
+import com.tid.StockMaster.services.auth.ApplicationUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,16 +13,22 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private ApplicationUserDetailsService userDetailsService;
+
     @Bean
     public SecurityFilterChain security(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize-> authorize.requestMatchers("/authenticate/**").permitAll()
                         .anyRequest().authenticated());
-
-
-
        return http.build();
+    }
+
+    @Bean
+    public ApplicationUserDetailsService userDetailsService() {
+        return userDetailsService;
     }
 
 }
---
+++ src/main/java/com/tid/StockMaster/services/auth/ApplicationUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.tid.StockMaster.services.auth;
+import com.tid.StockMaster.exception.EntityNotFoundException;
+import com.tid.StockMaster.exception.ErrorCodes;
+import com.tid.StockMaster.model.Utilisateur;
+import com.tid.StockMaster.repository.UtilisateurRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.Collections;
+
+@Service
+public class ApplicationUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UtilisateurRepository utilisateurRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Utilisateur utilisateur = utilisateurRepository.findByEmail(email).orElseThrow(
+                () -> new EntityNotFoundException("L'utilisateur n'existe pas", ErrorCodes.UTILISATEUR_NOT_FOUND)
+        );
+        return new User(utilisateur.getEmail(),utilisateur.getMoteDePasse(), Collections.emptyList());
+    }
+}
