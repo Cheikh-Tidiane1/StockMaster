aa4c15137957cc8eec3f87dc6053cc7a20634143
cheikh tidiane <cheikh951812@gmail.com>
2024-07-04 00:28:23 +0200

    feat: add ExtendedUser

--- src/main/java/com/tid/StockMaster/services/auth/ApplicationUserDetailsService.java
+++ src/main/java/com/tid/StockMaster/services/auth/ApplicationUserDetailsService.java
@@ -3,6 +3,7 @@ import com.tid.StockMaster.dto.UtilisateurDto;
 import com.tid.StockMaster.exception.EntityNotFoundException;
 import com.tid.StockMaster.exception.ErrorCodes;
 import com.tid.StockMaster.model.Utilisateur;
+import com.tid.StockMaster.model.auth.ExtendedUser;
 import com.tid.StockMaster.repository.UtilisateurRepository;
 import com.tid.StockMaster.services.UtilisateurService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +33,6 @@ public class ApplicationUserDetailsService implements UserDetailsService {
         UtilisateurDto utilisateur = utilisateurService.findByEmail(email);
         List<SimpleGrantedAuthority> authorities = new ArrayList<>();
         utilisateur.getRoles().forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getRoleName())));
-        return new User(utilisateur.getEmail(),utilisateur.getMoteDePasse(), Collections.emptyList());
+        return new ExtendedUser(utilisateur.getEmail(),utilisateur.getMoteDePasse(), utilisateur.getEntreprise().getId(),authorities);
     }
 }
