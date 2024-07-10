ef0658973ede4e966dbf9ee18a01bcbad6da0407
cheikh tidiane <cheikh951812@gmail.com>
2024-07-10 12:58:15 +0200

    feat(handlers): BadCredentialsException

--- src/main/java/com/tid/StockMaster/handlers/RestExceptionHandler.java
+++ src/main/java/com/tid/StockMaster/handlers/RestExceptionHandler.java
@@ -3,9 +3,11 @@ package com.tid.StockMaster.handlers;
 import java.util.Collections;
 
 import com.tid.StockMaster.exception.EntityNotFoundException;
+import com.tid.StockMaster.exception.ErrorCodes;
 import com.tid.StockMaster.exception.InvalidEntityException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -36,6 +38,20 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
                 .httpCode(badRequest.value())
                 .message(exception.getMessage())
                 .errors(exception.getErrors())
+                .build();
+
+        return new ResponseEntity<>(errorDto, badRequest);
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorDto> handleException(BadCredentialsException exception, WebRequest webRequest) {
+        final HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+
+        final ErrorDto errorDto = ErrorDto.builder()
+                .code(ErrorCodes.BAD_CREDENTIALS)
+                .httpCode(badRequest.value())
+                .message(exception.getMessage())
+                .errors(Collections.singletonList("Login et / ou mot de passe incorrecte"))
                 .build();
 
         return new ResponseEntity<>(errorDto, badRequest);
