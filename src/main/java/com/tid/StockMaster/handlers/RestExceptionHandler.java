9d4f07ea03a32e00c0df41503153b3728c02b1a5
cheikh tidiane <cheikh951812@gmail.com>
2024-07-17 00:11:59 +0200

    feat: InvalidOperationException

--- src/main/java/com/tid/StockMaster/handlers/RestExceptionHandler.java
+++ src/main/java/com/tid/StockMaster/handlers/RestExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import com.tid.StockMaster.exception.EntityNotFoundException;
 import com.tid.StockMaster.exception.ErrorCodes;
 import com.tid.StockMaster.exception.InvalidEntityException;
+import com.tid.StockMaster.exception.InvalidOperationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -20,6 +21,19 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<ErrorDto> handleException(EntityNotFoundException exception, WebRequest webRequest) {
 
         final HttpStatus notFound = HttpStatus.NOT_FOUND;
+        final ErrorDto errorDto = ErrorDto.builder()
+                .code(exception.getErrorCode())
+                .httpCode(notFound.value())
+                .message(exception.getMessage())
+                .build();
+
+        return new ResponseEntity<>(errorDto, notFound);
+    }
+
+    @ExceptionHandler(InvalidOperationException.class)
+    public ResponseEntity<ErrorDto> handleException(InvalidOperationException exception, WebRequest webRequest) {
+
+        final HttpStatus notFound = HttpStatus.BAD_REQUEST;
         final ErrorDto errorDto = ErrorDto.builder()
                 .code(exception.getErrorCode())
                 .httpCode(notFound.value())
