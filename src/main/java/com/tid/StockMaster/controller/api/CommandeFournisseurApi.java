9f45cb60ce48634f905f09a2057b177f2210524b
cheikh tidiane <cheikh951812@gmail.com>
2024-07-21 15:23:06 +0200

    feat(controller): update commandeFournisseur

--- src/main/java/com/tid/StockMaster/controller/api/CommandeFournisseurApi.java
+++ src/main/java/com/tid/StockMaster/controller/api/CommandeFournisseurApi.java
@@ -1,10 +1,11 @@
 package com.tid.StockMaster.controller.api;
 import com.tid.StockMaster.dto.CommandeFournisseurDto;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import com.tid.StockMaster.dto.LigneCommandeFournisseurDto;
+import com.tid.StockMaster.model.EtatCommande;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 import static com.tid.StockMaster.utils.Constants.APP_ROOT;
 
@@ -24,4 +25,24 @@ public interface CommandeFournisseurApi {
 
     @DeleteMapping(APP_ROOT + "/commandesfournisseurs/delete/{idCommandeFournisseur}")
     void delete(@PathVariable("idCommandeFournisseur") Integer id);
+
+    @PatchMapping(APP_ROOT + "/update/etat/{idCommande}/{etatCommande}")
+    CommandeFournisseurDto updateEtatCommande(@PathVariable("idCommande") Integer idCommande, @PathVariable("etatCommande") EtatCommande etatCommande);
+
+    @PatchMapping(APP_ROOT + "/update/quantite/{idCommande}/{idLigneCommande}/{quantite}")
+    CommandeFournisseurDto updateQuantiteCommande(@PathVariable("idCommande") Integer idCommande,
+                                                  @PathVariable("idLigneCommande") Integer idLigneCommande, @PathVariable("quantite") BigDecimal quantite);
+
+    @PatchMapping(APP_ROOT + "/update/fournisseur/{idCommande}/{idFournisseur}")
+    CommandeFournisseurDto updateFournisseur(@PathVariable("idCommande") Integer idCommande, @PathVariable("idFournisseur") Integer idFournisseur);
+
+    @PatchMapping(APP_ROOT + "/update/article/{idCommande}/{idLigneCommande}/{idArticle}")
+    CommandeFournisseurDto updateArticle(@PathVariable("idCommande") Integer idCommande,
+                                         @PathVariable("idLigneCommande") Integer idLigneCommande, @PathVariable("idArticle") Integer idArticle);
+
+    @DeleteMapping(APP_ROOT + "/delete/article/{idCommande}/{idLigneCommande}")
+    CommandeFournisseurDto deleteArticle(@PathVariable("idCommande") Integer idCommande, @PathVariable("idLigneCommande") Integer idLigneCommande);
+
+    @GetMapping(APP_ROOT + "/lignesCommande/{idCommande}")
+    List<LigneCommandeFournisseurDto> findAllLignesCommandesFournisseurByCommandeFournisseurId(@PathVariable("idCommande") Integer idCommande);
 }
