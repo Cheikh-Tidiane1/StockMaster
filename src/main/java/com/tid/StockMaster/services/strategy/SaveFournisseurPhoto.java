70e92fa7ed617f8a97d7ecf4b01d2006f03c4dc6
cheikh tidiane <cheikh951812@gmail.com>
2024-07-22 23:37:09 +0200

    feat(services): add saveUserPhoto

--- src/main/java/com/tid/StockMaster/services/strategy/SaveFournisseurPhoto.java
+++ src/main/java/com/tid/StockMaster/services/strategy/SaveFournisseurPhoto.java
@@ -1,14 +1,37 @@
 package com.tid.StockMaster.services.strategy;
+import com.flickr4java.flickr.FlickrException;
+import com.tid.StockMaster.dto.FournisseurDto;
+import com.tid.StockMaster.exception.ErrorCodes;
+import com.tid.StockMaster.exception.InvalidOperationException;
 import com.tid.StockMaster.model.Fournisseur;
+import com.tid.StockMaster.services.FlickrService;
+import com.tid.StockMaster.services.FournisseurService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
 import java.io.InputStream;
 
 @Service
 @Slf4j
-public class SaveFournisseurPhoto implements Strategy<Fournisseur> {
+public class SaveFournisseurPhoto implements Strategy<FournisseurDto> {
+    private FlickrService flickrService;
+    private FournisseurService fournisseurService;
+
+    @Autowired
+    public SaveFournisseurPhoto(FlickrService flickrService, FournisseurService fournisseurService) {
+        this.flickrService = flickrService;
+        this.fournisseurService = fournisseurService;
+    }
     @Override
-    public Fournisseur savePhoto(Integer id, InputStream photo, String title) {
-        return null;
+    public FournisseurDto savePhoto(Integer id, InputStream photo, String title) throws FlickrException {
+        FournisseurDto fournisseur = fournisseurService.findById(id);
+        String urlPhoto = flickrService.savePhoto(photo, title);
+        if (!StringUtils.hasLength(urlPhoto)) {
+            throw new InvalidOperationException("Erreur lors de l'enregistrement de photo du fournisseur", ErrorCodes.UPDATE_PHOTO_EXCEPTION);
+        }
+        fournisseur.setPhoto(urlPhoto);
+        return fournisseurService.save(fournisseur);
     }
 }
--- src/main/java/com/tid/StockMaster/services/strategy/SaveUtilisateurPhoto.java
+++ src/main/java/com/tid/StockMaster/services/strategy/SaveUtilisateurPhoto.java
@@ -1,14 +1,38 @@
 package com.tid.StockMaster.services.strategy;
+import com.flickr4java.flickr.FlickrException;
+import com.tid.StockMaster.dto.UtilisateurDto;
+import com.tid.StockMaster.exception.ErrorCodes;
+import com.tid.StockMaster.exception.InvalidOperationException;
 import com.tid.StockMaster.model.Utilisateur;
+import com.tid.StockMaster.services.FlickrService;
+import com.tid.StockMaster.services.UtilisateurService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
 import java.io.InputStream;
 
 @Service
 @Slf4j
-public class SaveUtilisateurPhoto implements Strategy<Utilisateur> {
+public class SaveUtilisateurPhoto implements Strategy<UtilisateurDto> {
+    private FlickrService flickrService;
+    private UtilisateurService utilisateurService;
+
+    @Autowired
+    public SaveUtilisateurPhoto(FlickrService flickrService, UtilisateurService utilisateurService) {
+        this.flickrService = flickrService;
+        this.utilisateurService = utilisateurService;
+    }
+
     @Override
-    public Utilisateur savePhoto(Integer id, InputStream photo, String title) {
-        return null;
+    public UtilisateurDto savePhoto(Integer id, InputStream photo, String title) throws FlickrException {
+        UtilisateurDto utilisateur = utilisateurService.findById(id);
+        String urlPhoto = flickrService.savePhoto(photo, title);
+        if (!StringUtils.hasLength(urlPhoto)) {
+            throw new InvalidOperationException("Erreur lors de l'enregistrement de photo de l'utilisateur", ErrorCodes.UPDATE_PHOTO_EXCEPTION);
+        }
+        utilisateur.setPhoto(urlPhoto);
+        return utilisateurService.save(utilisateur);
     }
 }
