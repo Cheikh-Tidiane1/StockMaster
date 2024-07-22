06c60dcfed84ef12344d23f08d41b98acb2ac5e6
cheikh tidiane <cheikh951812@gmail.com>
2024-07-22 23:33:12 +0200

    feat(services): add saveEntreprisePhoto

--- src/main/java/com/tid/StockMaster/services/strategy/SaveEntreprisePhoto.java
+++ src/main/java/com/tid/StockMaster/services/strategy/SaveEntreprisePhoto.java
@@ -1,15 +1,39 @@
 package com.tid.StockMaster.services.strategy;
+import com.flickr4java.flickr.FlickrException;
+import com.tid.StockMaster.dto.EntrepriseDto;
+import com.tid.StockMaster.exception.ErrorCodes;
+import com.tid.StockMaster.exception.InvalidOperationException;
 import com.tid.StockMaster.model.Entreprise;
+import com.tid.StockMaster.services.EntrepriseService;
+import com.tid.StockMaster.services.FlickrService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
 import java.io.InputStream;
 
 @Service
 @Slf4j
-public class SaveEntreprisePhoto implements Strategy<Entreprise> {
+public class SaveEntreprisePhoto implements Strategy<EntrepriseDto> {
+
+    private FlickrService flickrService;
+    private EntrepriseService entrepriseService;
+
+    @Autowired
+    public SaveEntreprisePhoto(FlickrService flickrService, EntrepriseService entrepriseService) {
+        this.flickrService = flickrService;
+        this.entrepriseService = entrepriseService;
+    }
 
     @Override
-    public Entreprise savePhoto(Integer id, InputStream photo, String title) {
-        return null;
+    public EntrepriseDto savePhoto(Integer id, InputStream photo, String title) throws FlickrException {
+        EntrepriseDto entreprise = entrepriseService.findById(id);
+        String urlPhoto = flickrService.savePhoto(photo, title);
+        if (!StringUtils.hasLength(urlPhoto)) {
+            throw new InvalidOperationException("Erreur lors de l'enregistrement de photo de l'entreprise", ErrorCodes.UPDATE_PHOTO_EXCEPTION);
+        }
+        entreprise.setPhoto(urlPhoto);
+        return entrepriseService.save(entreprise);
     }
 }
