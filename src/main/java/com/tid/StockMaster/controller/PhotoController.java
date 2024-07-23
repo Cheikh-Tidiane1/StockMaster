f1e9900f4544390fa3b22de175b7064424437960
cheikh tidiane <cheikh951812@gmail.com>
2024-07-23 08:35:06 +0200

    feat(controller): add photoController

---
+++ src/main/java/com/tid/StockMaster/controller/PhotoController.java
@@ -0,0 +1,25 @@
+package com.tid.StockMaster.controller;
+
+import com.flickr4java.flickr.FlickrException;
+import com.tid.StockMaster.controller.api.PhotoApi;
+import com.tid.StockMaster.services.strategy.StrategyPhotoContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+public class PhotoController implements PhotoApi {
+
+    private StrategyPhotoContext strategyPhotoContext;
+    @Autowired
+    public PhotoController(StrategyPhotoContext strategyPhotoContext) {
+        this.strategyPhotoContext = strategyPhotoContext;
+    }
+
+    @Override
+    public Object savePhoto(String context, Integer id, MultipartFile photo, String title) throws IOException, FlickrException {
+        return strategyPhotoContext.savePhoto(context,id,photo.getInputStream(),title);
+    }
+}
